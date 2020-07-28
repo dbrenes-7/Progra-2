6c2bccad85118f548464f6d919fbbb99dc378272
dbrenes-7 <66794156+dbrenes-7@users.noreply.github.com>
2020-07-28 11:16:38 -0600

    Add files via upload

--- funciones.c
+++ funciones.c
@@ -17,6 +17,7 @@ Arista{
     Nodo*vrt;
     Arista*siguiente;
     int peso;
+    int tiempo;
 };
 
 Lista{
@@ -35,25 +36,24 @@ void visualizarGrafo();
 void recorridos();
 void recorridoAnchura();
 void recorridoProfundidad(Nodo* aux);
-int buscarNodo(Nodo* aux,char buscado);
 void insertarPila(Nodo* aux);
 void insertarCola(Nodo* aux);
 Nodo*desencolar();
 void reiniciar();
-void dijkstra();
+void simulacion_Vehiculo_Taxi();
+int configuracion_Inicial();
 
 int main(){
     int opcion;
-    Nodo*aux=inicio;
     do{
-        printf("-----------\n");
+        printf("-------------------------------------\n");
         printf(" 1. Insertar Vertice                 \n");
         printf(" 2. Insertar Arista                  \n");
         printf(" 3. Mostrar Listas                   \n");
         printf(" 4. Recorrer Grafo                   \n");
-        printf(" 5. Dijkstra                         \n");
+        printf(" 5. Simulaciones                     \n");
         printf(" 6. Salir                            \n");
-        printf("------------\n");
+        printf("-------------------------------------\n");
         printf("Escoge opcion: ");
         scanf("%d",&opcion);
 
@@ -61,8 +61,8 @@ int main(){
         else if (opcion==2){insertarArista();}
         else if (opcion==3){visualizarGrafo();}
         else if(opcion==4){recorridos();}
-        else if (opcion==5){dijkstra();}
-        else if (opcion==6){ buscarNodo(aux,"A");}
+        else if (opcion==5){configuracion_Inicial();}
+        else if (opcion==6){ return 1;}
     }while(opcion<=6);
 
 
@@ -74,7 +74,7 @@ void insertarNodo(){
     Nodo* aux;
     Nodo* nuevo=(Nodo*)malloc(sizeof(Nodo));
     fflush(stdin);
-    printf("Ingrese vertice:");
+    printf("\nIngrese vertice:");
     scanf("%c",&nuevo->dato);
     nuevo->siguiente=NULL;
     nuevo->adyacencia=NULL;
@@ -100,14 +100,18 @@ void insertarArista(){
     Nodo* aux;
     Nodo* aux2;
     if(inicio==NULL){
-        printf("Error: El grafo est� vacio\n");
+        printf("\n--------------------------\n");
+        printf("\nError: El grafo esta vacio\n");
+        printf("\n--------------------------\n");
         return;
     }
     fflush(stdin);
-    printf("Ingresar Nodo Inicial y Final:");
+    printf("\nIngresar Nodo Inicial y Final:");
     scanf("%c %c",&ini,&fin);
-    printf("Ingresar Peso de la arista:");
+    printf("\nIngresar Peso de la arista:");
     scanf("%i",&nuevo->peso);
+    printf("\nIngresar el Tiempo de la arista:");
+    scanf("%i",&nuevo->tiempo);
     aux=inicio;
     aux2=inicio;
     while(aux2!=NULL){
@@ -117,7 +121,9 @@ void insertarArista(){
         aux2=aux2->siguiente;
     }
     if(aux2==NULL){
-        printf("Error:Vertice no encontrado\n");
+        printf("\n---------------------\n");
+        printf("\nVertice no encontrado\n");
+        printf("\n---------------------\n");
         return;
     }
     while(aux!=NULL){
@@ -128,7 +134,9 @@ void insertarArista(){
         aux = aux->siguiente;
     }
     if(aux==NULL)
-        printf("Error:Vertice no encontrado\n");
+        printf("\n---------------------\n");
+        printf("\nVertice no encontrado\n");
+        printf("\n---------------------\n");
 }
 
 void agregarArista(Nodo* aux,Nodo* aux2,Arista* nuevo){
@@ -169,7 +177,7 @@ void recorridos(){
     Nodo*aux=inicio,*aux2=inicio;
     if(inicio!=NULL){
         fflush(stdin);
-        printf("Escoger vertice inicial:");
+        printf("\nEscoger vertice inicial:");
         scanf("%c",&vertice);
         while(aux!=NULL){
             if(aux->dato==vertice)
@@ -177,7 +185,9 @@ void recorridos(){
             aux=aux->siguiente;
         }
         if(aux==NULL){
-            printf("Error: Vertice no encontrado\n");
+            printf("\n---------------------\n");
+            printf("\nVertice no encontrado\n");
+            printf("\n---------------------\n");
         }else{
             printf("Recorrido por anchura: ");
             aux->visitado=1;
@@ -306,11 +316,13 @@ void reiniciar(){
     }
 }
 //Lorem Ipsum
-void dijkstra(){
+void simulacion_Vehiculo_Taxi(int maxPasajeros){
     Nodo*aux=inicio;
     char a,b;
+    int tiempo=0,distancia=0;
+    int random_Pasajeros=rand()%maxPasajeros;
     fflush(stdin);
-    printf("Ingresar Nodo Inicial y Final:");
+    printf("\nIngresar punto de incio y destino:");
     scanf("%c %c",&a,&b);
     while(aux!=NULL){
         if(aux->dato==a){
@@ -321,7 +333,9 @@ void dijkstra(){
         aux=aux->siguiente;
     }
     if(aux==NULL){
-        printf("Vertice no encontrado\n");
+        printf("\n---------------------\n");
+        printf("\nVertice no encontrado\n");
+        printf("\n---------------------\n");
         return;
     }
     while(aux!=NULL){
@@ -330,6 +344,8 @@ void dijkstra(){
             if(a->vrt->monto==-1 || (aux->monto+a->peso)<a->vrt->monto){
                 a->vrt->monto=aux->monto+a->peso;
                 a->vrt->anterior=aux->dato;
+                distancia = distancia + a->peso;
+                tiempo = tiempo + a->tiempo;
             }
             a=a->siguiente;
         }
@@ -356,9 +372,106 @@ void dijkstra(){
         }
     }
     insertarPila(aux);
+    printf("\nRecorrido\n");
     while(ini!=NULL){
         printf("%c ",desencolar()->dato);
     }
     printf("\n");
+    printf("Distancia del viaje: %d\n",distancia);
+    printf("Tiempo de viaje: %d\n",tiempo);
+    printf("Numero de pasajeros: %d\n",random_Pasajeros);
     reiniciar();
+}
+
+void simulacion_Bus_Tren(int maxPasajeros){
+    Nodo*aux=inicio;
+    char a,b;
+    int tiempo=0,distancia=0,random;
+    int pasajeros_entrada=rand()%maxPasajeros,pasajeros_salida=0;
+    fflush(stdin);
+    printf("\nIngresar punto de incio y destino:");
+    scanf("%c %c",&a,&b);
+    while(aux!=NULL){
+        if(aux->dato==a){
+            aux->terminado=1;
+            aux->monto=0;
+            break;
+        }
+        aux=aux->siguiente;
+    }
+    if(aux==NULL){
+        printf("\n---------------------\n");
+        printf("\nVertice no encontrado\n");
+        printf("\n---------------------\n");
+        return;
+    }
+    while(aux!=NULL){
+        Arista*a=aux->adyacencia;
+        while(a!=NULL){
+            if(a->vrt->monto==-1 || (aux->monto+a->peso)<a->vrt->monto){
+                a->vrt->monto=aux->monto+a->peso;
+                a->vrt->anterior=aux->dato;
+                distancia = distancia + a->peso;
+                tiempo = tiempo + a->tiempo;
+                printf("\nIniciaron %d pasajeros",pasajeros_entrada);
+                pasajeros_salida=rand()%(maxPasajeros-pasajeros_entrada);
+                printf("\nSe bajaron %d pasajeros",pasajeros_salida);
+                pasajeros_entrada = (pasajeros_entrada - pasajeros_salida);
+                random = rand()%(maxPasajeros-pasajeros_entrada);
+                printf("\nSe subieron %d pasajeros\n", random);
+                pasajeros_entrada = pasajeros_entrada + random;
+
+            }
+            a=a->siguiente;
+        }
+        aux=inicio;
+        Nodo*min=inicio;
+        while(min->anterior==0 || min->terminado ==1)
+            min=min->siguiente;
+        while(aux!=NULL){
+            if(aux->monto<min->monto && aux->terminado==0 && aux->anterior!=0)
+                min=aux;
+            aux=aux->siguiente;
+        }
+        aux=min;
+        aux->terminado=1;
+        if(aux->dato==b)
+            break;
+    }
+    while(aux->anterior!=0){
+        insertarPila(aux);
+        char temp=aux->anterior;
+        aux=inicio;
+        while(aux->dato!=temp){
+            aux=aux->siguiente;
+        }
+    }
+    insertarPila(aux);
+    printf("\nRecorrido\n");
+    while(ini!=NULL){
+        printf("%c ",desencolar()->dato);
+    }
+    printf("\n");
+    printf("Distancia del viaje: %d\n",distancia);
+    printf("Tiempo de viaje: %d\n",tiempo);
+    printf("LLegaron %d pasajeros al destino\n",pasajeros_entrada);
+    reiniciar();
+}
+
+int configuracion_Inicial(){
+    int opcion, maxPasajeros, numSimulaciones;
+    printf("\n[1]Tren\n[2]Bus\n[3]Taxi\n[4]Vehiculo personal\nElija el metodo de transporte: ");
+    scanf("%d",&opcion);
+    printf("\nIndique la cantidad maxima de pasajeros del medio de transporte: ");
+    scanf("%d",&maxPasajeros);
+    printf("\nIngrese la cantidad de simulaciones que vaya a realizar: ");
+    scanf("%d",&numSimulaciones);
+
+    for (int i = 0; i < numSimulaciones; ++i) {
+        if(opcion==1){simulacion_Bus_Tren(maxPasajeros);}
+        else if (opcion==2){simulacion_Bus_Tren(maxPasajeros);}
+        else if (opcion==3){simulacion_Vehiculo_Taxi(maxPasajeros);}
+        else if (opcion==4){simulacion_Vehiculo_Taxi(maxPasajeros);}
+    }
+    return 0;
 }
